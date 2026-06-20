a8a2de20243f9307a02ca79ffb5bee78d35fa62f
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 13:55:18 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,51 +2,29 @@
 // ZSWAP PLUS - LOGIN
 // ===================================
 
-
 import { auth } from "/Zswap-plus/firebase/firebase.js";
 
-
 import {
-
 signInWithEmailAndPassword
-
 }
-
 from
-
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-
-
 
 
 const loginForm = document.getElementById("loginForm");
 
 
-
-
-
 loginForm.addEventListener("submit", async(e)=>{
-
 
 e.preventDefault();
 
 
-
-
-
 const email =
-
 document.getElementById("email").value.trim();
 
 
-
-
 const password =
-
 document.getElementById("password").value;
-
-
 
 
 
@@ -54,41 +32,10 @@ try{
 
 
 await signInWithEmailAndPassword(
-
 auth,
-
 email,
-
 password
-
 );
-
-
-
-
-
-// Save login details
-
-sessionStorage.setItem(
-
-"zswapEmail",
-
-email
-
-);
-
-
-
-
-sessionStorage.setItem(
-
-"zswapName",
-
-email.split("@")[0]
-
-);
-
-
 
 
 
@@ -96,29 +43,27 @@ alert("Login successful");
 
 
 
+// Clear old profile cache
+
+sessionStorage.clear();
 
 
-// Go to ZSwap Plus home page
 
-window.location.href =
+// Open home
 
-"../pages/home.html";
-
-
+window.location.assign(
+"../pages/home.html"
+);
 
 
 
 }
-
 
 catch(error){
 
-
 alert(error.message);
 
-
 }
-
 
 
 });
