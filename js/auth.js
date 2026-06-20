3887a20831601c193505d1f9334fb74819e3a037
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 07:53:00 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,19 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
 
+    e.preventDefault();
+
+    const country = document.getElementById("countryCode").value;
+    const phone = document.getElementById("phoneNumber").value;
+
+    if(phone.length < 7){
+        alert("Enter a valid phone number");
+        return;
+    }
+
+    // Save temporarily
+    localStorage.setItem("zswapPhone", country + phone);
+
+    // Go to verification page
+    window.location.href = "verify.html";
+
+});
