85b7d552f9954d5545d98c3894ba60aeaa890375
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 07:44:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
+// ===============================
+// ZSWAP PLUS APP
+// ===============================
 
+console.log("ZSwap Plus Loaded");
+
+// Welcome animation
+window.addEventListener("load", () => {
+
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        document.body.style.transition = "0.5s";
+        document.body.style.opacity = "1";
+    }, 100);
+
+});
+
+// Feature cards hover effect
+
+const cards =
+document.querySelectorAll(".feature-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+
+        card.style.transform = "translateY(-5px)";
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform = "translateY(0px)";
+
+    });
+
+});
+
+// Future ZSwap Notifications
+
+function showNotification(message){
+
+    alert(message);
+
+}
