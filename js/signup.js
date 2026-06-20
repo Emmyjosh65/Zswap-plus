dd86f29b9d5e0bef5d5512a2ef62d0449877f819
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 19:58:21 +0100

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,10 @@
-// ===================================
-// ZSWAP PLUS - SIGNUP WITH FIRESTORE
-// ===================================
-
-
 import { auth, db } from "/Zswap-plus/firebase/firebase.js";
-
 
 import {
 createUserWithEmailAndPassword
 }
 from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 
 import {
 doc,
@@ -20,147 +13,71 @@ setDoc
 from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
-
-
-
 const signupForm = document.getElementById("signupForm");
-
-
 
 if (signupForm) {
 
-
-signupForm.addEventListener("submit", async (e)=>{
-
+signupForm.addEventListener("submit", async (e) => {
 
 e.preventDefault();
 
-
-
-
 const fullName =
-
 document.getElementById("fullName").value.trim();
 
-
-
 const email =
-
 document.getElementById("email").value.trim();
 
-
-
 const password =
-
 document.getElementById("password").value;
 
-
-
 const confirmPassword =
-
 document.getElementById("confirmPassword").value;
 
-
-
-
-
 if(password !== confirmPassword){
-
 alert("Passwords do not match");
-
 return;
-
 }
-
-
-
 
 if(password.length < 6){
-
 alert("Password must be at least 6 characters");
-
 return;
-
 }
-
-
-
-
 
 try {
 
-
-// Create Firebase account
-
 const userCredential =
-
 await createUserWithEmailAndPassword(
-
 auth,
-
 email,
-
 password
-
 );
-
-
 
 const user = userCredential.user;
 
-
-
-
-
-// Save user profile
-
 await setDoc(
-
 doc(db, "users", user.uid),
-
 {
-
-name: fullName,
-
-email: email,
-
 uid: user.uid,
-
+name: fullName,
+email: email,
+photoURL: "../assets/default-avatar.png",
 createdAt: new Date()
-
 }
-
 );
-
-
-
-
 
 alert("ZSwap account created successfully");
 
-
-
 window.location.href =
-
-"../pages/profile.html";
-
-
-
+"../auth/login.html";
 
 }
 
 catch(error){
 
-
 alert(error.message);
-
 
 }
 
-
-
 });
-
 
 }
