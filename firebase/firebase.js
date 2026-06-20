12d96b5e47847eaba70b317fe84b51b97919d830
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 10:30:10 +0100

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,24 +1,57 @@
-// Firebase setup
+// ===================================
+// ZSWAP PLUS - FIREBASE CONFIG
+// ===================================
+
 
 import { initializeApp } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from
+
+import { getAuth } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
 
 
 const firebaseConfig = {
 
+
   apiKey: "AIzaSyCy8VqqsgpDKpa0FkHmOKqM6EjLAOFlqiA",
+
 
   authDomain: "zswap-plus.firebaseapp.com",
 
-  projectId: "zswap-plus
+
+  projectId: "zswap-plus",
+
 
   storageBucket: "zswap-plus.firebasestorage.app",
 
+
   messagingSenderId: "810316847652",
 
-  appId: "YOUR_APP_ID"
+
+  appId: "1:810316847652:web:4e23d267ca21e45aded6b1",
+
+
+  measurementId: "G-BVC97F73TT"
+
 
 };
+
+
+
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+// Firebase Authentication
+
+export const auth = getAuth(app);
