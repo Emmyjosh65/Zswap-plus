3160b8b1df305f4287be74eb879d907f968c7688
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 09:18:32 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,23 @@
 // ===================================
 // ZSWAP PLUS - FIREBASE PHONE LOGIN
 // ===================================
+
+
 console.log("ZSwap Firebase Auth Loaded");
+
+
+
 import { auth } from "../firebase/firebase.js";
 
 
 import {
     RecaptchaVerifier,
     signInWithPhoneNumber
-} 
-from 
+}
+from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
 
 
 
@@ -18,17 +25,38 @@ const loginForm =
 document.getElementById("loginForm");
 
 
+const continueBtn =
+document.getElementById("continueBtn");
 
-// Firebase Recaptcha
+
+
+
+
+// Create Firebase Recaptcha
 
 window.recaptchaVerifier =
 new RecaptchaVerifier(
+
     auth,
+
     "recaptcha-container",
+
     {
-        size: "invisible"
+
+        size: "invisible",
+
+        callback: () => {
+
+            console.log("Recaptcha verified");
+
+        }
+
     }
+
 );
+
+
+
 
 
 
@@ -45,12 +73,14 @@ document.getElementById("countryCode").value;
 
 
 const phoneNumber =
-document.getElementById("phoneNumber").value;
+document.getElementById("phoneNumber").value.trim();
 
 
 
 const fullPhoneNumber =
 countryCode + phoneNumber;
+
+
 
 
 
@@ -64,38 +94,65 @@ return;
 
 
 
+
 try{
+
+
+continueBtn.innerText = "Sending...";
+
+continueBtn.disabled = true;
+
+
+
 
 
 const confirmationResult =
 
 await signInWithPhoneNumber(
+
     auth,
+
     fullPhoneNumber,
+
     window.recaptchaVerifier
+
 );
 
 
 
-// Save phone number
+
+
+// Save phone
 
 sessionStorage.setItem(
+
     "zswapPhone",
+
     fullPhoneNumber
+
 );
+
+
 
 
 
 // Save verification ID
 
 sessionStorage.setItem(
+
     "verificationId",
+
     confirmationResult.verificationId
+
 );
 
 
 
+
+
 alert("OTP sent successfully");
+
+
 
 
 
@@ -108,7 +165,19 @@ window.location.href =
 
 catch(error){
 
+
+console.error(error);
+
+
 alert(error.message);
+
+
+
+continueBtn.innerText = "Continue";
+
+continueBtn.disabled = false;
+
+
 
 }
 
