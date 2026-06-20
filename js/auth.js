389ea8868242d8b3cbd56346339f6cdc4875127d
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 08:23:50 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,19 +1,62 @@
-document.getElementById("loginForm").addEventListener("submit", function(e){
+import { auth } from "../firebase/firebase.js";
+
+import {
+    RecaptchaVerifier,
+    signInWithPhoneNumber
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const form = document.getElementById("loginForm");
+
+
+window.recaptchaVerifier = new RecaptchaVerifier(
+    auth,
+    "loginForm",
+    {
+        size: "invisible"
+    }
+);
+
+
+form.addEventListener("submit", async (e)=>{
 
     e.preventDefault();
 
-    const country = document.getElementById("countryCode").value;
-    const phone = document.getElementById("phoneNumber").value;
 
-    if(phone.length < 7){
-        alert("Enter a valid phone number");
-        return;
+    const country =
+    document.getElementById("countryCode").value;
+
+
+    const phone =
+    document.getElementById("phoneNumber").value;
+
+
+    const fullNumber = country + phone;
+
+
+    try{
+
+        const confirmation =
+        await signInWithPhoneNumber(
+            auth,
+            fullNumber,
+            window.recaptchaVerifier
+        );
+
+
+        window.confirmationResult = confirmation;
+
+
+        alert("OTP sent successfully");
+
+
+        window.location.href="verify.html";
+
+
+    }catch(error){
+
+        alert(error.message);
+
     }
-
-    // Save temporarily
-    localStorage.setItem("zswapPhone", country + phone);
-
-    // Go to verification page
-    window.location.href = "verify.html";
 
 });
