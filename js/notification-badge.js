80d7a5c1ae7b90d8d0e5af407271c5a965b78787
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 21:08:46 +0100

    Update notification-badge.js

--- js/notification-badge.js
+++ js/notification-badge.js
@@ -1,1 +1,104 @@
+// ===================================
+// ZSWAP PLUS - NOTIFICATION BADGE
+// ===================================
 
+
+import { auth, db } from "/Zswap-plus/firebase/firebase.js";
+
+
+import {
+
+onAuthStateChanged
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+import {
+
+collection,
+query,
+where,
+onSnapshot
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+
+
+const badge =
+document.getElementById("notificationBadge");
+
+
+
+
+
+onAuthStateChanged(auth,(user)=>{
+
+
+if(!user){
+
+return;
+
+}
+
+
+
+
+
+const notificationsQuery = query(
+
+collection(db,"notifications"),
+
+where("userId","==",user.uid)
+
+);
+
+
+
+
+
+onSnapshot(notificationsQuery,(snapshot)=>{
+
+
+const count =
+snapshot.size;
+
+
+
+
+if(count > 0){
+
+
+badge.innerText = count;
+
+
+badge.style.display = "block";
+
+
+}
+
+
+else{
+
+
+badge.style.display = "none";
+
+
+}
+
+
+
+});
+
+
+
+});
