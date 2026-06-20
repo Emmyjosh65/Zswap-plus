13a452d8e9a8caf7d3f43a277717f60ac2b579d3
Emmyjosh65 <ge5853987@gmail.com>
2026-06-20 08:14:24 +0100

    Create firebase.js

---
+++ firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCy8VqqsgpDKpa0FkHmOKqM6EjLAOFlqiA",
+  authDomain: "zswap-plus.firebaseapp.com",
+  projectId: "zswap-plus",
+  storageBucket: "zswap-plus.firebasestorage.app",
+  messagingSenderId: "810316847652",
+  appId: "1:810316847652:web:4e23d267ca21e45aded6b1",
+  measurementId: "G-BVC97F73TT"
+};
